961c781d0ee8d98b6817042346b2fd8259c12ebf
Andrew Peirs <peirs.andrew@gmail.com>
2025-02-07 17:06:02 +0000

    Fixed column

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -4,14 +4,16 @@ import Item from "./Item";
 // AP-TODO - Show how I would manage a nav that is custom depending on user roles
 const Nav: React.FC = () => {
   return (
-    <nav className="min-w-[320px] h-[100vh] bg-[#EFEFEF] pt-[96px]">
-      <ul>
-        <Item to="/">Dashboard</Item>
-        <Item to="/people">People</Item>
-        <Item to="/settings">Settings</Item>
-        <Item to="/help">Help</Item>
-      </ul>
-    </nav>
+    <div className="min-w-[320px]">
+      <nav className="fixed min-w-[320px] h-[100vh] bg-[#EFEFEF] pt-[96px]">
+        <ul>
+          <Item to="/">Dashboard</Item>
+          <Item to="/people">People</Item>
+          <Item to="/settings">Settings</Item>
+          <Item to="/help">Help</Item>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
